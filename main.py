b549d4564cbbd161896c38cc2bcb8da9f92e4ffb
kanta <kanta510g@outlook.jp>
2021-09-19 01:34:31 +0900

    mtml output

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from generater import *
 import re
 
-f = open(r"txt_to_HTML\generater.py",encoding="utf-8")
+f = open(r"txt_to_HTML\test_input.txt",encoding="utf-8")
+html = open(r"txt_to_HTML\text_out.html", "w")
 
-print("<html><head><title>...</title><head></body>")
+print("<html><head><title>...</title><head></body>", file=html)
 
 title = True
 for block in blocks(f):
     if title:
-        print("<h1>")
-        print(block)
-        print("</h1>")
+        print("<h1>", file=html)
+        print(block, file=html)
+        print("</h1>", file=html)
         title = False
     else:
-        print("<p>")
-        print(block)
-        print("</p>")
+        print("<p>", file=html)
+        print(block, file=html)
+        print("</p>", file=html)
 
 
 
-print("</body></html>")
+print("</body></html>", file=html)
