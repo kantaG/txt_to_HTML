831821f667f6fb51c3bf0a7f91acadf4bb168663
kanta <kanta510g@outlook.jp>
2021-09-18 22:28:05 +0900

    add line generator

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello,world")
+from generater import *
+
+f = open(r"C:\Users\kanta\Documents\programming\python\txt_to_HTML\test_input.txt",encoding="utf-8")
+
+for line in blocks(f):
+    print(line)
