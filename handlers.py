d2eaffccbbd2b572aa3a760520a1d4a595aa03f4
kanta <kanta510g@outlook.jp>
2021-09-21 18:22:22 +0900

    add handler superclass

--- handlers.py
+++ handlers.py
@@ -1,10 +1,28 @@
-class HTMLrender:
+class Handler:
+    def callback(self, prefix, name, *args):
+        method = getattr(self, prefix + name, None)
+        if callable(method):
+            return method(*args)
+    def start(self, name):
+        self.callback('start_', name)
+    def end(self, name):
+        self.callback('end_', name)
+    def sub(self, name):
+        def subtituation(match):
+            result = self.callback('sub_', name)
+            if result is None:
+                match.group(0)
+            return result
+        return subtituation
+    
+
+
+class HTMLrender(Handler):
     def start_paragraph(self):
         print("<p>")
     def end_paragraph(self):
         print("</p>")
-    def emphsis(self, line):
+    def sub_emphsis(self, line):
         return f'<em>{line.group(1)}</em>'
     def feed(self, data):
         print(data)
-    
