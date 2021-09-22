1280e91fa2a2cfaad2a6b19c9ec6af73e014d157
kanta <kanta510g@outlook.jp>
2021-09-22 23:55:47 +0900

    create parser

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello,world")
+import re
+from utils import *
+from handlers import *
+from rules import *
+
+class Parser:
+    def __init__(self, handler):
+        self.handler = handler
+        self.rules = []
+        self.filters = []
+    def addRule(self, rule):
+        self.rules.append(rule)
+    def addFilter(self, filter):
+        def filter(block, handler):
+            return re.sub(pattern, handler.sub(name), block)
+        self.filters.append(filter)
+    
+    def parse(self, file):
+        self.handler.start("docment")
+        for block in blocks(file):
+            for filter in self.filters:
+                block = filter(block, self.handler)
+            for rule in self.rules:
+                if rule.condition(block):
+                    last = rule.action(block, self.handler)
+                    if last:
+                        break
+        self.handler.end('docment')
