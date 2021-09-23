b16e98bec53f7adc9ae57e8bb47386fbafb67b7e
kanta <kanta510g@outlook.jp>
2021-09-24 01:54:55 +0900

    add handlers

--- handlers.py
+++ handlers.py
@@ -18,11 +18,35 @@ class Handler:
 
 
 class HTMLrender(Handler):
+    def start_document(self):
+        print('<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>...</title></head><body>')
+    def end_documenmt(self):
+        print("</body0></html>")
     def start_paragraph(self):
         print("<p>")
     def end_paragraph(self):
         print("</p>")
-    def sub_emphsis(self, line):
-        return f'<em>{line.group(1)}</em>'
+    def start_title(self):
+        print("<h1>")
+    def end_title(self):
+        print("</h1>")
+    def start_heading(self):
+        print("<h2>")
+    def end_heading(self):
+        print("</h2>")
+    def start_list(self):
+        print("<ul>")
+    def end_list(self):
+        print("</ul>")
+    def start_listitem(self):
+        print("<li>")
+    def end_listitem(self):
+        print("</li>")
+    def sub_emphsis(self, match):
+        return f'<em>{match.group(1)}</em>'
+    def sub_url(self, match):
+        return f'<a harf="{match.group(1)}">{match.group(1)}</a>'
+    def sub_mail(self, match):
+        return f'<a harf="mailto:{match.group(1)}">{match.group(1)}</a>'
     def feed(self, data):
         print(data)
